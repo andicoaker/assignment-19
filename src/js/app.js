7406dc453a2d0ec7f7109ff1bc5e20ca761a9fa9
andicoaker <Andi@Andreas-MacBook.local>
2017-02-10 12:22:55 -0500

    initial files

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,8 @@
-console.log('wat')
+// console.log('wat')
+import { githubApiKey } from '../../secrets.js'
+// console.log(githubApiKey);
+import $ from 'jquery'
+
+$.getJSON('https://api.github.com/users/t3patterson?access_token=4c9db64304527b11846e4c28bbf5d26be5700853').then(function(serverRes){
+    console.log(serverRes);
+})
